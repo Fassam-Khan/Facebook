25dd54ab8dac16b7862d61477e0f9c1bc50c84de
Fassam Khan <muhammadfassam465@gmail.com>
2026-02-13 00:23:05 +0500

    Add new CSS styles for layout and post box, and implement story data in app.js

--- app.js
+++ app.js
@@ -7,4 +7,66 @@ if(notLog == null){
 
 }
 
+const stories = [
+    {
+      id: 1,
+      username: "Ali Khan",
+      profilePic: "https://randomuser.me/api/portraits/men/32.jpg",
+      storyImage: "https://picsum.photos/id/1011/400/700",
+    },
+    {
+      id: 2,
+      username: "Sara Ahmed",
+      profilePic: "https://randomuser.me/api/portraits/women/44.jpg",
+      storyImage: "https://picsum.photos/id/1025/400/700",
+    },
+    {
+      id: 3,
+      username: "Usman Tariq",
+      profilePic: "https://randomuser.me/api/portraits/men/76.jpg",
+      storyImage: "https://picsum.photos/id/1005/400/700",
+    },
+    {
+      id: 4,
+      username: "Ayesha Malik",
+      profilePic: "https://randomuser.me/api/portraits/women/65.jpg",
+      storyImage: "https://picsum.photos/id/1012/400/700",
+    },
+    {
+      id: 5,
+      username: "Hamza Sheikh",
+      profilePic: "https://randomuser.me/api/portraits/men/11.jpg",
+      storyImage: "https://picsum.photos/id/1035/400/700",
+    },
+    {
+      id: 6,
+      username: "Zainab Noor",
+      profilePic: "https://randomuser.me/api/portraits/women/21.jpg",
+      storyImage: "https://picsum.photos/id/1041/400/700",
+    },
+    {
+      id: 7,
+      username: "Bilal Hussain",
+      profilePic: "https://randomuser.me/api/portraits/men/54.jpg",
+      storyImage: "https://picsum.photos/id/1062/400/700",
+    },
+    {
+      id: 8,
+      username: "Hira Salman",
+      profilePic: "https://randomuser.me/api/portraits/women/33.jpg",
+      storyImage: "https://picsum.photos/id/1074/400/700",
+    }
+  ];
 
+  let storyStyle = document.getElementById("storiesBox")
+
+  storyStyle.innerHTML += stories.map((story)=>{
+    return `  <div class="story" style="background-image: url(${story.storyImage});">
+    <div class="story-profile">
+    </div>
+    <div class="story-title">
+        <p>${story.username} </p>
+    </div>
+
+</div>`
+  }).join("")
