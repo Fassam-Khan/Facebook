7bb53da49260e16a52094e7ad3197e5d97c13ae6
Fassam Khan <muhammadfassam465@gmail.com>
2026-03-06 23:22:52 +0500

    Refactor login functionality and update UI elements in index and login pages

--- login/login.js
+++ login/login.js
@@ -2,28 +2,36 @@ let email = document.getElementById("email");
 let password = document.getElementById("password");
 const isLogin = false
 let userEmail = JSON.parse(localStorage.getItem("users"))
-console.log(userEmail);
+let loginBtn = document.getElementById("login");
 
-
-
-const login = ()=>{
-
-    userEmail.forEach(element => {
-        if(element.email == email.value && element.password == password.value){
-            alert("Login Successfully")
-            notLog = JSON.parse(localStorage.getItem("notLogin") || [])
-            localStorage.setItem("notLogin",JSON.stringify(element))
-            window.location.href = "/"
-            
-        }
-        else{
-            alert("Login unsuccessfully")
-        }
-        
-    });
-    if(email.value.trim() == ""  || password.value.trim() == ""){
-        alert("invalid Crediantial")
+const login = () => {
+    if (userEmail) {
+        userEmail?.forEach(element => {
+            if (element.email == email.value && element.password == password.value) {
+                alert("Login Successfully")
+                localStorage.setItem("notLogin", JSON.stringify(element))
+                window.location.href = "/"
+                notLog = JSON.parse(localStorage.getItem("notLogin") || [])
+                localStorage.setItem("notLogin", JSON.stringify(element))
+            }
+            else {
+                alert("Login unsuccessfully")
+            }
+        });
 
     }
-    
+    if(userEmail == null || userEmail == undefined){
+        alert("Please Register First ")
+    }
+    if (email.value.trim() == "" || password.value.trim() == "") {
+        alert("Please Enter Email and Password")
+
+    }
+  
+
+
+
 }
+
+
+loginBtn.addEventListener("click", login);
